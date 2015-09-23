8cca4856826c72b7fd169cb23483f16f5c400a71
Ben A. Morgan <ben@benmorgan.io>
2015-09-22 17:46:49 -0700

    add render instance and render collection method and deprecate the index and show

--- app/controllers/spree/api/v2/base_controller.rb
+++ app/controllers/spree/api/v2/base_controller.rb
@@ -5,12 +5,32 @@ module Spree
         protected
 
         def index(collection = [])
+          ActiveSupport::Deprecation.warn <<-TEXT
+            Using `super` is now deprecated. We're aiming to removing as much
+            meta-programming as possible. Please use the `render_collection`
+            method instead. This will be removed in 0.3.0.
+          TEXT
+
+          render_collection collection
+        end
+
+        def show(object = {})
+          ActiveSupport::Deprecation.warn <<-TEXT
+            Using `super` is now deprecated. We're aiming to removing as much
+            meta-programming as possible. Please use the `render_instance`
+            method instead. This will be removed in 0.3.0.
+          TEXT
+
+          render_instance object
+        end
+
+        def render_collection(collection = [])
           collection = collection.where(filter_params).paginate(page_params)
           render json: collection, include: params[:include],
                  meta: { page: page_details(collection) }
         end
 
-        def show(object = {})
+        def render_instance(object = {})
           render json: object, include: params[:include]
         end
 
