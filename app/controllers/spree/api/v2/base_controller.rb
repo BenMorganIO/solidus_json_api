c1df7887a6d6cdbd9bc744ac4a067482c9f51b55
Ben A. Morgan <ben@benmorgan.io>
2015-09-22 17:49:14 -0700

    squish the deprecation warning

--- app/controllers/spree/api/v2/base_controller.rb
+++ app/controllers/spree/api/v2/base_controller.rb
@@ -5,7 +5,7 @@ module Spree
         protected
 
         def index(collection = [])
-          ActiveSupport::Deprecation.warn <<-TEXT
+          ActiveSupport::Deprecation.warn <<-TEXT.squish
             Using `super` is now deprecated. We're aiming to removing as much
             meta-programming as possible. Please use the `render_collection`
             method instead. This will be removed in 0.3.0.
@@ -15,7 +15,7 @@ module Spree
         end
 
         def show(object = {})
-          ActiveSupport::Deprecation.warn <<-TEXT
+          ActiveSupport::Deprecation.warn <<-TEXT.squish
             Using `super` is now deprecated. We're aiming to removing as much
             meta-programming as possible. Please use the `render_instance`
             method instead. This will be removed in 0.3.0.
