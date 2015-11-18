4585f07e305260951e5c2412c802785cd25dd5eb
Ben A. Morgan <ben@benmorgan.io>
2015-11-18 14:25:34 -0800

    add index action

--- app/controllers/spree/api/v2/orders_controller.rb
+++ app/controllers/spree/api/v2/orders_controller.rb
@@ -2,8 +2,18 @@ module Spree
   module Api
     module V2
       class OrdersController < Spree::Api::V2::BaseController
+        def index
+          render_collection orders
+        end
+
         def show
-          render_instance @current_api_user.orders.find(params[:id])
+          render_instance orders.find(params[:id])
+        end
+
+        private
+
+        def orders
+          @current_api_user.admin? ? Spree::Order : @current_api_user.orders
         end
       end
     end
