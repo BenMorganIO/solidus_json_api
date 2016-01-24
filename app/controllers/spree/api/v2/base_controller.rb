c2c05b4ea1c7698f7319667f048b42859e2cb3d7
Ben A. Morgan <ben@benmorgan.io>
2016-01-24 00:52:44 -0800

    remove deprecated 0.2.0 methods
    These were used in 0.1, deprecated in 0.2, and now that we’re moving on
    to 0.3, I think its safe to remove now.

--- app/controllers/spree/api/v2/base_controller.rb
+++ app/controllers/spree/api/v2/base_controller.rb
@@ -3,28 +3,6 @@ module Spree
     module V2
       class BaseController < Spree::Api::BaseController
         include Spree::Api::V2::Renderable
-
-        protected
-
-        def index(collection = [])
-          ActiveSupport::Deprecation.warn <<-TEXT.squish
-            Using `super` is now deprecated. We're aiming to removing as much
-            meta-programming as possible. Please use the `render_collection`
-            method instead. This will be removed in 0.3.0.
-          TEXT
-
-          render_collection collection
-        end
-
-        def show(object = {})
-          ActiveSupport::Deprecation.warn <<-TEXT.squish
-            Using `super` is now deprecated. We're aiming to removing as much
-            meta-programming as possible. Please use the `render_instance`
-            method instead. This will be removed in 0.3.0.
-          TEXT
-
-          render_instance object
-        end
       end
     end
   end
